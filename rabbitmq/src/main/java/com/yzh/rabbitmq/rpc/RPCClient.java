54acb97981e8c2fae6b446eba562c13d9d983143
yuanzhihao <742599783@qq.com>
2020-12-01 23:06:06 +0800

    提交RPC测试方法

--- rabbitmq/src/main/java/com/yzh/rabbitmq/rpc/RPCClient.java
+++ rabbitmq/src/main/java/com/yzh/rabbitmq/rpc/RPCClient.java
@@ -1,15 +1,56 @@
 package com.yzh.rabbitmq.rpc;
 
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.UUID;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.TimeoutException;
+
 /**
  * RPC客户端
- *
+ *Ø
  * @author yuanzhihao
  * @since 2020/11/21
  */
+
 public class RPCClient {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, TimeoutException, InterruptedException {
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        connectionFactory.setHost("192.168.1.108");
+        connectionFactory.setPort(5672);
+        connectionFactory.setVirtualHost("/");
 
+        try ( Connection connection = connectionFactory.newConnection();
+              Channel channel = connection.createChannel()) {
+            // 声明一个队列
+            String queueName = "test_rpc";
 
+            String corrId = UUID.randomUUID().toString();
+            String replayQueueName = channel.queueDeclare().getQueue();
+            AMQP.BasicProperties properties = new AMQP.BasicProperties
+                    .Builder()
+                    .correlationId(corrId)
+                    .replyTo(replayQueueName)
+                    .build();
+            String msg = "hello rpc";
+            channel.basicPublish("",queueName,properties,msg.getBytes());
+            final BlockingQueue<String> response = new ArrayBlockingQueue<>(1);
+
+            String ctag = channel.basicConsume(replayQueueName, true, (consumerTag, message) -> {
+                if (message.getProperties().getCorrelationId().equals(corrId)) {
+                    response.offer(new String(message.getBody(), StandardCharsets.UTF_8));
+                }
+            }, consumerTag -> {
+            });
+
+            // 获取响应结果
+            String take = response.take();
+            System.out.println("rpc result is "+ take);
+            channel.basicCancel(ctag);
+        }
     }
 }
--- rabbitmq/src/main/java/com/yzh/rabbitmq/rpc/RPCServer.java
+++ rabbitmq/src/main/java/com/yzh/rabbitmq/rpc/RPCServer.java
@@ -1,5 +1,11 @@
 package com.yzh.rabbitmq.rpc;
 
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.TimeoutException;
+
 /**
  * RPC服务端
  *
@@ -8,7 +14,60 @@ package com.yzh.rabbitmq.rpc;
  */
 public class RPCServer {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, TimeoutException {
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        connectionFactory.setHost("192.168.1.108");
+        connectionFactory.setPort(5672);
+        connectionFactory.setVirtualHost("/");
 
+        //获得connection
+        Connection connection = connectionFactory.newConnection();
+        //获得channel对象
+        Channel channel = connection.createChannel();
+
+        String queueName = "test_rpc";
+        channel.queueDeclare(queueName, false, false, false, null);
+
+
+        Object monitor = new Object();
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            AMQP.BasicProperties replyProps = new AMQP.BasicProperties
+                    .Builder()
+                    .correlationId(delivery.getProperties().getCorrelationId())
+                    .build();
+
+            String response = "";
+            try {
+                String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
+                System.out.println("toUpperCase(" + message + ")");
+                response = toUpperCase(message);
+            } catch (RuntimeException e) {
+                System.out.println(e.toString());
+            } finally {
+                channel.basicPublish("", delivery.getProperties().getReplyTo(), replyProps, response.getBytes(StandardCharsets.UTF_8));
+                channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
+                // RabbitMq consumer worker thread notifies the RPC server owner thread
+                synchronized (monitor) {
+                    monitor.notify();
+                }
+            }
+        };
+
+        channel.basicConsume(queueName, false, deliverCallback, (consumerTag -> { }));
+        // Wait and be prepared to consume the message from RPC client.
+        while (true) {
+            synchronized (monitor) {
+                try {
+                    monitor.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    // 提供一个大小写转换的方法
+    private static String toUpperCase(String msg) {
+        return msg.toUpperCase();
     }
 }

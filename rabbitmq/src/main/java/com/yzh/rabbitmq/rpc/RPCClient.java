68e1b47570972fdf928208ce04e14f6b9285b985
yuanzhihao <742599783@qq.com>
2021-01-09 12:57:41 +0800

    RPC调用增加注释

--- rabbitmq/src/main/java/com/yzh/rabbitmq/rpc/RPCClient.java
+++ rabbitmq/src/main/java/com/yzh/rabbitmq/rpc/RPCClient.java
@@ -11,7 +11,7 @@ import java.util.concurrent.TimeoutException;
 
 /**
  * RPC客户端
- *Ø
+ *
  * @author yuanzhihao
  * @since 2020/11/21
  */
@@ -19,6 +19,7 @@ import java.util.concurrent.TimeoutException;
 public class RPCClient {
 
     public static void main(String[] args) throws IOException, TimeoutException, InterruptedException {
+        // 创建connection以及channel对象
         ConnectionFactory connectionFactory = new ConnectionFactory();
         connectionFactory.setHost("192.168.1.108");
         connectionFactory.setPort(5672);
@@ -29,23 +30,29 @@ public class RPCClient {
             // 声明一个队列
             String queueName = "test_rpc";
 
+            // 请求消息中需要带一个唯一标识ID
             String corrId = UUID.randomUUID().toString();
+            // 声明一个回调队列
             String replayQueueName = channel.queueDeclare().getQueue();
+            // 将correlationId以及回调队列设置在消息的属性中
             AMQP.BasicProperties properties = new AMQP.BasicProperties
                     .Builder()
                     .correlationId(corrId)
                     .replyTo(replayQueueName)
                     .build();
+            // 具体消息内容
             String msg = "hello rpc";
+            // 发送请求消息
             channel.basicPublish("",queueName,properties,msg.getBytes());
+            // 设置一个阻塞队列  等待服务端的响应
             final BlockingQueue<String> response = new ArrayBlockingQueue<>(1);
 
             String ctag = channel.basicConsume(replayQueueName, true, (consumerTag, message) -> {
+                // 注意 这边根据correlationId进行下判断
                 if (message.getProperties().getCorrelationId().equals(corrId)) {
                     response.offer(new String(message.getBody(), StandardCharsets.UTF_8));
                 }
-            }, consumerTag -> {
-            });
+            }, consumerTag -> {});
 
             // 获取响应结果
             String take = response.take();
--- rabbitmq/src/main/java/com/yzh/rabbitmq/rpc/RPCServer.java
+++ rabbitmq/src/main/java/com/yzh/rabbitmq/rpc/RPCServer.java
@@ -15,22 +15,22 @@ import java.util.concurrent.TimeoutException;
 public class RPCServer {
 
     public static void main(String[] args) throws IOException, TimeoutException {
+        // 首先还是正常获得connection以及channel对象
         ConnectionFactory connectionFactory = new ConnectionFactory();
         connectionFactory.setHost("192.168.1.108");
         connectionFactory.setPort(5672);
         connectionFactory.setVirtualHost("/");
-
-        //获得connection
         Connection connection = connectionFactory.newConnection();
-        //获得channel对象
         Channel channel = connection.createChannel();
 
+        // 定义一个rpc的队列
         String queueName = "test_rpc";
         channel.queueDeclare(queueName, false, false, false, null);
 
-
         Object monitor = new Object();
+        // 具体的消费代码里面实现
         DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            // 消费者将请求消息中的correlationId信息再作为响应传回replyTo队列
             AMQP.BasicProperties replyProps = new AMQP.BasicProperties
                     .Builder()
                     .correlationId(delivery.getProperties().getCorrelationId())
@@ -38,14 +38,18 @@ public class RPCServer {
 
             String response = "";
             try {
+                // 提供一个大小写转换的方法
                 String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
                 System.out.println("toUpperCase(" + message + ")");
                 response = toUpperCase(message);
             } catch (RuntimeException e) {
                 System.out.println(e.toString());
             } finally {
+                // 将响应传回replyTo队列
                 channel.basicPublish("", delivery.getProperties().getReplyTo(), replyProps, response.getBytes(StandardCharsets.UTF_8));
+                // 设置了手动应答 需要手动确认消息
                 channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
+                // 执行完成会释放主线程的锁
                 // RabbitMq consumer worker thread notifies the RPC server owner thread
                 synchronized (monitor) {
                     monitor.notify();
@@ -53,7 +57,9 @@ public class RPCServer {
             }
         };
 
+        // 监听"test_rpc"队列
         channel.basicConsume(queueName, false, deliverCallback, (consumerTag -> { }));
+        // 这个锁对象是确保我们server的调用逻辑执行完成 首先挂起主线程
         // Wait and be prepared to consume the message from RPC client.
         while (true) {
             synchronized (monitor) {

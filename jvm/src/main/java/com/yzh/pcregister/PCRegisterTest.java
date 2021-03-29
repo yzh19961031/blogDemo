d7a1c18e05a94a26ea39248b5af0535b8101371f
yuanzhihao <742599783@qq.com>
2021-03-29 22:36:31 +0800

    添加PC寄存器测试代码

---
+++ jvm/src/main/java/com/yzh/pcregister/PCRegisterTest.java
@@ -0,0 +1,16 @@
+package com.yzh.pcregister;
+
+/**
+ * pc寄存器测试
+ *
+ * @author yuanzhihao
+ * @since 2021/3/26
+ */
+public class PCRegisterTest {
+
+    public static void main(String[] args) {
+        int i = 10;
+        int j = 20;
+        int k = i + j;
+    }
+}

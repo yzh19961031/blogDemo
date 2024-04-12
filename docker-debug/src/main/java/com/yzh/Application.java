56c98bac112cc6835107ffd17ff51be5be34894d
yuanzhihao <742599783@qq.com>
2024-04-12 15:51:28 +0800

    远程调试Docker容器内的Java进程

---
+++ docker-debug/src/main/java/com/yzh/Application.java
@@ -0,0 +1,30 @@
+package com.yzh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 启动类
+ *
+ * @author yuanzhihao
+ * @since 2024/4/12
+ */
+@RestController
+@RequestMapping
+@SpringBootApplication
+public class Application {
+
+    @GetMapping("/hello/{name}")
+    public String hello(@PathVariable("name") String name) {
+        return "Hello " + name;
+    }
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
